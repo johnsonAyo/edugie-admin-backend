f5c758f054a11d2b0784b43f842fe3ae6268e9cd
ambiti0n <fabulousfresh010@gmail.com>
2022-04-15 03:01:34 +0100

    updated Api

---
+++ server/routes/mealRoutes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+// import imageMulter from "./../utils/multerImageUpload";
+import {
+  deleteMeal,
+  updateMeal,
+  createMeal,
+  getAllMeal,
+  getMeal,
+} from "./../controllers/mealController";
+
+const router = express.Router();
+
+// router.route("/").get(getAllMeal).post(imageMulter.single("productThumbnail"), createMeal);
+router.route("/").get(getAllMeal).post(createMeal);
+router.route("/:id").get(getMeal).delete(deleteMeal).patch(updateMeal);
+
+export default router;
